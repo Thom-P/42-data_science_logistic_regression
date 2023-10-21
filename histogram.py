b45936a1daba136a28fee6f58cc19fe4d6c09788
tplanes <tho.planes@gmail.com>
2023-10-21 14:00:53 +0200

    refactored histogram

--- histogram.py
+++ histogram.py
@@ -1,46 +1,52 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from load_csv import ft_load
 
 
+def make_figure(courses, house_names, house_dfs):
+    """Prepare and plot the histogram array"""
+    colors = list(mcolors.BASE_COLORS.keys())
+
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(3, 5, figsize=(19, 10))  # size in 100s of pixs
+    n_bins = 10
+
+    for index, course in enumerate(courses):
+        scores = [house_dfs[h][course].dropna().to_numpy()
+                  for h in range(len(house_names))]
+
+        ax_curr = ax.flatten()[index]
+        #  nb: remove stacked=True for non-stacked version
+        ax_curr.hist(scores, n_bins, histtype='bar', stacked=True,
+                     label=house_names, color=colors[:len(house_names)])
+        ax_curr.set_title(course)
+        ax_curr.set_xlabel('Score')
+        ax_curr.set_ylabel('# of students')
+        if index == 0:
+            ax_curr.legend()
+    ax[-1, -1].axis('off')  # turn off last two subplots (unused)
+    ax[-1, -2].axis('off')
+    fig.tight_layout()
+    fig.savefig('histogram.png')
+    # plt.show()
+
+
 def main():
+    """Make a histogram array of all course score distributions"""
     try:
         df = ft_load('datasets/dataset_train.csv')
-        # separate into the 4 houses
-        rav = df[df['Hogwarts House'] == 'Ravenclaw']
-        sly = df[df['Hogwarts House'] == 'Slytherin']
-        gry = df[df['Hogwarts House'] == 'Gryffindor']
-        huf = df[df['Hogwarts House'] == 'Hufflepuff']
 
-        plt.style.use('dark_background')
-        fig, ax = plt.subplots(3, 5, figsize=(19, 10))  # size in 100s of pixs
-        n_bins = 10
-        course_list = [col for col in df if df[col].dtype == np.float64]
-        print(course_list)
-        print(len(course_list))
-        for index, course in enumerate(course_list):
-            scores = [rav[course].dropna().to_numpy(),
-                      sly[course].dropna().to_numpy(),
-                      gry[course].dropna().to_numpy(),
-                      huf[course].dropna().to_numpy()]
+        # get list of courses
+        courses = [col for col in df if df[col].dtype == np.float64]
 
-            ax_curr = ax.flatten()[index]
-            ax_curr.hist(scores, n_bins, histtype='bar', stacked=True,
-                         label=['Ravenclaw', 'Slytherin',
-                                'Gryffindor', 'Hufflepuff'])
-            #  non-stacked version
-            #  ax_curr.hist(scores, n_bins, histtype='bar')
-            ax_curr.set_title(course)
-            ax_curr.set_xlabel('Score')
-            ax_curr.set_ylabel('# of students')
-            if index == 0:
-                ax_curr.legend()
-        ax[-1, -1].axis('off')  # turn off last two subplots (unused)
-        ax[-1, -2].axis('off')
-        fig.tight_layout()
-        fig.savefig('histogram.png')
-        # plt.show()
+        # separate data into the 4 houses, and only keep courses columns
+        house_names = df['Hogwarts House'].unique()
+        house_dfs = [df.loc[df['Hogwarts House'] == name, courses]
+                     for name in house_names]
+
+        make_figure(courses, house_names, house_dfs)
         return 0
 
     except Exception as err:
