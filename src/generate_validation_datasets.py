8eb3d6342dabbe381baa4113c6aaad2810de57f0
tplanes <tho.planes@gmail.com>
2023-10-27 15:23:40 +0200

    refactored generate fake (now validation) datasets

---
+++ src/generate_validation_datasets.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import random
+import numpy as np
+import pandas as pd
+
+
+def split_dataset(df):
+    '''Separate original training data into train
+    and test subsets (80/20) for validation'''
+    n_total = df.shape[0]
+    n_train = round(80 / 100 * n_total)
+    n_test = n_total - n_train
+    ind_test = set(random.sample(range(n_total), n_test))
+    ind_train = set(range(n_total)) - ind_test
+    df_test = df.iloc[list(ind_test), :]
+    df_train = df.iloc[list(ind_train), :]
+    df_test.reset_index(drop=True, inplace=True)
+    df_train.reset_index(drop=True, inplace=True)
+
+    # Put houses (expected answers) from new test set in separate df
+    df_result = df_test['Hogwarts House'].copy()
+
+    # Remove houses from new test set
+    df_test.loc[:, 'Hogwarts House'] = np.nan
+
+    return df_train, df_test, df_result
+
+
+def save_validation_datasets(validation_dir, df_train, df_test, df_result):
+    # Save new validation datasets
+    df_train.to_csv(
+        os.path.join(validation_dir, 'dataset_train.csv'),
+        index_label='Index'
+    )
+    df_test.to_csv(
+        os.path.join(validation_dir, 'dataset_test.csv'),
+        index_label='Index'
+    )
+    df_result.to_csv(os.path.join(
+        validation_dir, 'expected_result.csv'), index_label='Index'
+    )
+    return
+
+
+def main():
+    '''Split original train dataset into subsets for validation'''
+    try:
+        random.seed(13)  # to keep things reproducible for now
+        df = pd.read_csv(
+            os.path.join('datasets', 'dataset_train.csv'), index_col=0
+        )
+
+        validation_dir = 'validation_datasets'
+        if not os.path.exists(validation_dir):
+            os.makedirs(validation_dir)
+        df_train, df_test, df_result = split_dataset(df)
+        save_validation_datasets(validation_dir, df_train, df_test, df_result)
+
+    except Exception as err:
+        print(f'{type(err).__name__}: {err}')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
